0b02352fd9d2d2bd688a659565813c13b3a5f947
Farshid Rayhan <32468549+farshidrayhanuiu@users.noreply.github.com>
2018-04-27 20:18:07 +0600

    Update Model.py

--- ROI/Model.py
+++ ROI/Model.py
@@ -14,8 +14,6 @@ from tflearn.layers.embedding_ops import embedding
 from tflearn.layers.merge_ops import merge
 
 from tflearn.objectives import softmax_categorical_crossentropy
-# from sklearn.tree import DecisionTreeClassifier
-
 
 
 class Model:
@@ -47,305 +45,35 @@ class Model:
     def load_model(self):
 
         network = input_data(shape=self.input_shape, name='input')
-        # network = conv_2d(network, 1, 1, activation='relu',  padding='valid', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 3, 3, activation='relu', padding='same', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 1, 1, activation='relu',  padding='valid', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 64, 3, activation='relu',  padding='same', strides=[1, 1, 1, 1])
-        # network = max_pool_2d(network, kernel_size=2, strides=2)
-
-        # network = conv_2d(network, 1, 1, activation='relu',  padding='valid', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 128, 3, activation='relu',  padding='same', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 1, 1, activation='relu',  padding='valid', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 128, 3, activation='relu',  padding='same', strides=[1, 1, 1, 1])
-        # network = max_pool_2d(network, kernel_size=2, strides=2)
-        #
-        # network = conv_2d(network, 1, 1, activation='relu',  padding='valid', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 256, 3, activation='relu',  padding='same', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 1, 1, activation='relu',  padding='valid', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 256, 3, activation='relu',  padding='same', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 1, 1, activation='relu',  padding='valid', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 256, 3, activation='relu',  padding='same', strides=[1, 1, 1, 1])
-        # network = max_pool_2d(network, kernel_size=2, strides=2)
-        #
-        #
-        # network = conv_2d(network, 1, 1, activation='relu', padding='valid', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 512, 3, activation='relu', padding='same', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 1, 1, activation='relu',  padding='valid', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 512, 3, activation='relu', padding='same', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 1, 1, activation='relu',  padding='valid', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 512, 3, activation='relu',  padding='same', strides=[1, 1, 1, 1])
-        # network = max_pool_2d(network, kernel_size=2, strides=2)
-
-        # network = conv_2d(network, 1, 1, activation='relu', padding='valid', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 512, 3, activation='relu', padding='same', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 1, 1, activation='relu', padding='valid', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 512, 3, activation='relu', padding='same', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 1, 1, activation='relu', padding='valid', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 512, 3, activation='relu', padding='same', strides=[1, 1, 1, 1])
-        # network = max_pool_2d(network, kernel_size=2, strides=2)
-        #
-        # print(network)
-        #
-        # network = fully_connected(network, 4096, activation='tanh')
-        # network = dropout(network, 0.5)
-        # network = fully_connected(network, 4096, activation='tanh')
-        # network = dropout(network, 0.5)
-        # network = fully_connected(network, 17, activation='softmax')
-        # network = fully_connected(network, 17, activation='softmax')
-        # print(network)
-        # network = tf.reshape(network, (-1, 16, 16, 4))
-        # print(network)
-        # encoder = tflearn.input_data(shape=[None, 784])
-        # network = tflearn.fully_connected(network, 256)
-        # network = tflearn.fully_connected(network, 64)
-        #
-        # Building the decoder
-        # network = tflearn.fully_connected(network, 256)
-        #
-        #
-        # network = conv_2d(network, 16, 1, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network = conv_2d(network, 16, 3, activation='relu', regularizer="L2", padding='same', strides=[1, 2, 2, 1])
-        # network = conv_2d(network, 32, 3, activation='relu', regularizer="L2", padding='same', strides=[1, 2, 2, 1])
-        # network2 = conv_2d(network, 16, 1, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network2 = conv_2d(network2, 32, 5, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network3 = conv_2d(network, 16, 1, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network3 = conv_2d(network3, 32, 3, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network = max_pool_2d(network, kernel_size=2, strides=2)
-
-        # network = conv_2d(network, 32, 2, activation='relu', regularizer="L2", padding='same', strides=[1, 2, 2, 1])
-        # network = conv_2d(network, 32, 3, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network = max_pool_2d(network, kernel_size=2, strides=2)
-        # network = conv_2d(network, 32, 2, activation='relu', regularizer="L2", padding='same', strides=[1, 2, 2, 1])
-        # network = conv_2d(network, 32, 3, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network = max_pool_2d(network, kernel_size=2, strides=2)
-        #
-        #
-        # network = merge([network1, network2, network3, network4] , mode='concat', axis=3)
-        # print(network)
-        #
-        # network90 = max_pool_2d(network, kernel_size=2, strides=1)
-        # #
-        # network = max_pool_2d(network, kernel_size=2, strides=2)
-        # print(network)
-        # network1 = conv_2d(network, 8, 1, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network1 = conv_2d(network1, 128, 2, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network2 = conv_2d(network, 8, 1, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network2 = conv_2d(network2, 128, 5, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network91 = merge([network1, network2] , mode='concat', axis=3)
-        #
-        # print('91 ',network)
-        # #
-        # network = max_pool_2d(network, kernel_size=1, strides=1)
-        # network3 = conv_2d(network, 8, 1, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network3 = conv_2d(network3, 128, 3, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network4 = max_pool_2d(network, kernel_size=2, strides=1)
-        # network5 = conv_2d(network, 8, 1, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network5 = conv_2d(network5, 128, 2, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        #
-        # network92 = merge([network3, network4, network5], mode='concat', axis=3)
-        #
-        # print('92 ',network)
-        #
-        # network = merge([network91, network92], mode='concat', axis=3)
-        # network93 = upsample_2d(network, kernel_size=2)
-        # #
-        # network = upsample_2d(network,kernel_size=2)
-        # print(network)
-        # network1 = conv_2d(network, 16, 3, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network1 = conv_2d(network1, 32, 3, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network2 = conv_2d(network, 16, 1, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network2 = conv_2d(network2, 32, 5, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network3 = conv_2d(network, 16, 1, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network3 = conv_2d(network3, 32, 2, activation='relu', regularizer="L2", padding='same', strides=[1, 1, 1, 1])
-        # network4 = max_pool_2d(network3, kernel_size=2, strides=1)
-        # network = merge([ network3, network4] , mode='concat', axis=3)
-        # print(network)
-        # network = conv_2d(network, 96, 11, strides=4, activation='relu')
-        # network = max_pool_2d(network, 3, strides=2)
-        # network = local_response_normalization(network)
-        # network = conv_2d(network, 256, 5, activation='relu')
-        # network = max_pool_2d(network, 3, strides=2)
-        # network = local_response_normalization(network)
-        # network = conv_2d(network, 384, 3, activation='relu')
-        # network = conv_2d(network, 384, 3, activation='relu')
-        # network = conv_2d(network, 256, 3, activation='relu')
-        # network = max_pool_2d(network, 3, strides=2)
-        # network = local_response_normalization(network)
-        # network = fully_connected(network, 4096, activation='tanh')
-        # network = dropout(network, 0.5)
-        # network = fully_connected(network, 4096, activation='tanh')
-        # network = dropout(network, 0.5)
-
-        # network = conv_2d(network, 64, 3, activation='relu')
-        # network = local_response_normalization(network)
-        # network = conv_2d(network, 64, 3, activation='relu')
-        # network = local_response_normalization(network)
-        # network = max_pool_2d(network, 2, strides=2)
-        #
-        # network = conv_2d(network, 128, 3, activation='relu')
-        # network = local_response_normalization(network)
-        # network = conv_2d(network, 128, 3, activation='relu')
-        # network = local_response_normalization(network)
-        # network = max_pool_2d(network, 2, strides=2)
-        #
-        # network = conv_2d(network, 256, 3, activation='relu')
-        # network = local_response_normalization(network)
-        # network = conv_2d(network, 256, 3, activation='relu')
-        # network = local_response_normalization(network)
-        # network = conv_2d(network, 256, 3, activation='relu')
-        # network = local_response_normalization(network)
-        # network = max_pool_2d(network, 2, strides=2)
-        #
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = local_response_normalization(network)
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = local_response_normalization(network)
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = local_response_normalization(network)
-        # network = max_pool_2d(network, 2, strides=2)
-        #
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = local_response_normalization(network)
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = local_response_normalization(network)
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = local_response_normalization(network)
-        # network = max_pool_2d(network, 2, strides=2)
-        # network = conv_2d(network, 64, 3, strides=4, activation='relu')
-        # network = max_pool_2d(network, 3, strides=2)
-        # network = local_response_normalization(network)
-        # tf.image.resize_bilinear()
-
-        # network1 = conv_2d(network, 64, 5, activation='relu')
-        # network1 = conv_2d(network1, 64, 5, activation='relu')
-        # network1 = conv_2d(network1, 64, 5, activation='relu')
-        # network2 = conv_2d(network, 64, 3, activation='relu')
-        # network2 = conv_2d(network2, 64, 3, activation='relu')
-        # network2 = conv_2d(network2, 64, 3, activation='relu')
-        # network = merge([network1, network2] , mode='concat', axis=3)
-        # network = max_pool_2d(network, 3, strides=2)
 
         network = self.inception(network, 64)
         network = conv_2d(network, 128, 3, activation='relu')
         network = conv_2d(network, 128, 3, activation='relu')
         network = self.inception(network,64,strides=2)
-        print(network)
-        # network = max_pool_2d(network, 2, strides=2)
-
-        # network1 = conv_2d(network, 128, 3, activation='relu')
-        # network2 = conv_2d(network, 128, 2, activation='relu')
-        # network = merge([network1, network2], mode='concat', axis=3)
-
-        # network = self.inception(network,64)
-        # network = self.inception(network,64,strides=2)
-        # print(network)
 
         network = conv_2d(network, 128, 3, activation='relu')
         network = conv_2d(network, 128, 3, activation='relu')
         network = conv_2d(network, 128, 3, activation='relu',strides=2)
         network = self.inception(network, 96)
-        print(network)
-
 
         network = conv_2d(network, 256, 3, activation='relu')
         network = conv_2d(network, 256, 3, activation='relu')
         network = conv_2d(network, 256, 3, activation='relu',strides=2)
         network = self.inception(network, 96)
-        print(network)
 
         network = conv_2d(network, 256, 3, activation='relu')
         network = conv_2d(network, 256, 3, activation='relu')
         network = conv_2d(network, 256, 3, activation='relu', strides=2)
         network = self.inception(network, 128)
 
-        print(network)
-
         network = conv_2d(network, 512, 3, activation='relu')
         network = conv_2d(network, 512, 3, activation='relu')
         network = conv_2d(network, 512, 3, activation='relu')
         network = self.inception(network, 128)
-        print(network)
-        # network = max_pool_2d(network, 2, strides=2)
 
-        # network = max_pool_2d(network, 3, strides=2)
-        #
-        # network1 = conv_2d(network, 512, 5, activation='relu')
-        # network1 = conv_2d(network1, 512, 5, activation='relu')
-        # network1 = conv_2d(network1, 512, 5, activation='relu')
-        # network2 = conv_2d(network, 512, 5, activation='relu')
-        # network2 = conv_2d(network2, 512, 5, activation='relu')
-        # network2 = conv_2d(network2, 512, 3, activation='relu')
-        # network = merge([network1, network2] , mode='concat', axis=3)
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = max_pool_2d(network, 2, strides=2)
-        #
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = max_pool_2d(network, 2, strides=2)
-        # network = tf.image.resize_images(network, size=(14,14), method=tf.image.ResizeMethod.BICUBIC)
-        # print(network)
-        #
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = upsample_2d(network, kernel_size=2)
-        # network = tf.image.resize_images(network, size=(28,28), method=tf.image.ResizeMethod.BILINEAR)
-        # print(network)
-
-        # network = conv_2d(network, 256, 3, activation='relu')
-        # network = conv_2d(network, 256, 3, activation='relu')
-        # network = tf.image.resize_images(network, size=(10,10), method=tf.image.ResizeMethod.BICUBIC)
-        # network = upsample_2d(network, kernel_size=2)
-
-        # network = conv_2d(network, 256, 3, activation='relu')
-        # network = conv_2d(network, 256, 3, activation='relu')
-        # network = tf.image.resize_images(network, size=(12,12), method=tf.image.ResizeMethod.BICUBIC)
-        # network = upsample_2d(network, kernel_size=2)
-        #
-        # network = conv_2d(network, 128, 3, activation='relu')
-        # network = conv_2d(network, 128, 3, activation='relu')
-        # network = tf.image.resize_images(network, size=(14,14), method=tf.image.ResizeMethod.BICUBIC)
-        # network = upsample_2d(network, kernel_size=2)
-
-        # network = conv_2d(network, 64, 3, activation='relu')
-        # network = conv_2d(network, 64, 3, activation='relu')
-        # network = max_pool_2d(network, 2, strides=2)
-        #
-        # network = conv_2d(network, 128, 3, activation='relu')
-        # network = conv_2d(network, 128, 3, activation='relu')
-        # network = max_pool_2d(network, 2, strides=2)
-        #
-        # network = conv_2d(network, 256, 3, activation='relu')
-        # network = conv_2d(network, 256, 3, activation='relu')
-        # network = conv_2d(network, 256, 3, activation='relu')
-        # network = max_pool_2d(network, 2, strides=2)
-        #
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = upsample_2d(network, kernel_size=2)
-
-        # network = self.inception(network, filter_num=128)
-        # network = self.inception(network, filter_num=128)
-        # network = self.inception(network, filter_num=128)
-        # network = max_pool_2d(network, 3, strides=2)
-
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = upsample_2d(network, kernel_size=2)
-
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = conv_2d(network, 512, 3, activation='relu')
-        # network = conv_2d(network, 368, 3, activation='relu')
-        # network = self.inception(network, filter_num=128)
-        # network = self.inception(network, filter_num=128)
-        # network = max_pool_2d(network, 3, strides=2)
-
-        network = fully_connected(network, 4096, activation='relu')
+        network = fully_connected(network, 512, activation='relu')
+        network = dropout(network, 0.8)
+        network = fully_connected(network, 6192, activation='relu')
         network = dropout(network, 0.8)
         network = fully_connected(network, 4096, activation='relu')
         network = dropout(network, 0.8)
